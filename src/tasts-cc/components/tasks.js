7788c0e29bb1970f828377ce51c1f234af681a09
Victor Martin <victor.martin.alvarez@oracle.com>
2020-07-09 21:40:05 +0200

    Add custom component code for tasks

--- src/tasts-cc/components/tasks.js
+++ src/tasts-cc/components/tasks.js
@@ -1,26 +1,52 @@
-'use strict';
- 
+"use strict";
+
+const fetch = require("node-fetch");
+
+const ordsURL = "<SODA_URL>/ords";
+const collection = "tasks";
+
+const username = "ADMIN";
+const password = "<ADMIN_PASSOWRD>";
+const authString = `${username}:${password}`;
+
+function getTasks(urlRequest, logger, callback) {
+  logger.info(urlRequest);
+  fetch(urlRequest)
+    .then((res) => {
+      if (!res.ok) {
+        const errorMessage = `Invalid status ${res.status}`;
+        logger.error(errorMessage);
+        callback(errorMessage);
+        throw new Error(errorMessage);
+      }
+      return res.json();
+    })
+    .then((body) => {
+      callback(null, body.items);
+    })
+    .catch((err) => {
+      logger.error(err.message);
+      callback(err.message);
+    });
+}
+
 module.exports = {
   metadata: () => ({
-    name: 'tasks',
-    properties: {
-      human: { required: true, type: 'string' },
-    },
-    supportedActions: ['weekday', 'weekend']
+    name: "com.example.tasks",
+    supportedActions: ["success", "failure"],
   }),
   invoke: (conversation, done) => {
-    // perform conversation tasks.
-    const { human } = conversation.properties();
-    // determine date
-    const now = new Date();
-    const dayOfWeek = now.toLocaleDateString('en-US', { weekday: 'long' });
-    const isWeekend = [0, 6].indexOf(now.getDay()) > -1;
-    // reply
-    conversation
-      .reply(`Greetings ${human}`)
-      .reply(`Today is ${now.toLocaleDateString()}, a ${dayOfWeek}`)
-      .transition(isWeekend ? 'weekend' : 'weekday');
- 
-    done();
-  }
+    const urlRequest = `https://${authString}@${ordsURL}/${username.toLowerCase()}/soda/latest/${collection}`;
+    getTasks(urlRequest, conversation.logger(), (err, items) => {
+      if (err) {
+        conversation.transition("failure");
+        done();
+        return;
+      }
+      const values = items.map((item) => item.value);
+      const tasks = values.map((v) => v.text);
+      conversation.reply(tasks.join("\n")).transition("success");
+      done();
+    });
+  },
 };
